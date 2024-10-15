d1c4ee2c79c320215906202ff261f7c2d321b3f2
Facundo Cozzani <fcozzani@cafemartinez.central>
2024-10-15 10:38:39 -0300

    Estilos css

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,29 +4,29 @@ export function Navbar () {
   return (
     <>
       <div className='bg-slate-800 h-full w-2/12'>
-        <ul className='flex flex-col p-2 text-xl'>
-          <li>
+        <ul className='flex flex-col text-xl text-center font-semibold'>
+          <li className='border border-slate-500 p-1'>
             <NavLink
               to='/'
-              className={({ isActive }) => isActive ? 'text-white' : 'text-green-600'}
-            >Fondos escalofriantes
+              className={({ isActive }) => isActive ? 'text-blue-500' : 'text-white'}
+            >Cambiar Fondo
             </NavLink>
           </li>
 
-          <li>
+          <li className='border border-slate-500 p-1'>
             <NavLink
               to='/monster'
-              className={({ isActive }) => isActive ? 'text-white' : 'text-green-600'}
+              className={({ isActive }) => isActive ? 'text-blue-500' : 'text-white'}
             >Agregar Monstruos
             </NavLink>
 
           </li>
 
-          <li>
+          <li className='border border-slate-500 p-1'>
             <NavLink
               to='/creatercart'
-              className={({ isActive }) => isActive ? 'text-white' : 'text-green-600'}
-            > Crear tarjeta de halloween
+              className={({ isActive }) => isActive ? 'text-blue-500' : 'text-white'}
+            > Agregar Texto
             </NavLink>
           </li>
         </ul>
--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -5,12 +5,14 @@ export function TopBar () {
 
   return (
     <>
-      <header className='w-full bg-gray-700 flex justify-evenly items-center text-xl p-2'>
-        <h1 className='font-semibold'>SpookySnap</h1>
+      <header className='w-full h-16 bg-slate-800 flex justify-center items-center text-xl p-2'>
+        <div className='w-2/3 flex justify-between'>
+          <h1 className='font-semibold text-2xl'>SpookySnap</h1>
 
-        <button className='p-1 pl-2 pr-2 bg-blue-700 rounded text-lg' onClick={uploadImg}>
+          <button className='p-1 pl-2 pr-2 bg-blue-700 rounded text-lg' onClick={uploadImg}>
           Subir Imagen
-        </button>
+          </button>
+        </div>
       </header>
     </>
   )
